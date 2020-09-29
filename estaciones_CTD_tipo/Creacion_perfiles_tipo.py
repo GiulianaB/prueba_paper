029acc73ac0561d22a53d438b2ff45c98b8b98ac
Giuliana <GiulianaB@GiulianaB.com>
2020-09-29 09:28:11 -0300

    [new CTD files .p]

--- estaciones_CTD_tipo/Creacion_perfiles_tipo.py
+++ estaciones_CTD_tipo/Creacion_perfiles_tipo.py
@@ -8,19 +8,29 @@ Perfiles CTD para probar en .csv
 """
 from seabird import fCNV
 import pandas as pd
+import pickle
 
 
 files = ['d01.cnv','d02.cnv','d040.cnv','d050.cnv']
-for f in files:
-    
-    CTD1 = fCNV(f)
 
-    d = {'LAT' : CTD1['LATITUDE'],
-         'LON' : CTD1['LONGITUDE'],
-         'PRES': CTD1['PRES'],
+for f in files:
+
+    CTD1 = fCNV(f)
+    nombre = f[:3]
+    fecha = str(CTD1.attributes['datetime'])[:10] # 'anio-mes-dia'
+    lon = CTD1['LONGITUDE'].data.mean()
+    lat = CTD1['LATITUDE'].data.mean()
+
+    d = {'PRES': CTD1['PRES'],
          'SAL' : CTD1['PSAL'],
          'TEMP': CTD1['TEMP']}
-    df = pd.DataFrame(data = d)
-    df.to_csv(f[:3]+'.csv')
-    
 
+    df = pd.DataFrame(data = d)
+
+    dic = {'estacion':nombre,
+            'fecha': fecha,
+            'lat':lat,
+            'lon':lon,
+            'data':df}
+
+    pickle.dump(dic, open(nombre+'.p', "wb" ) )
